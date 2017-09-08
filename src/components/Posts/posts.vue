a2e7bb54ce0cf6c456ef2fa359cd7dc66a8157e8
qq718121 <512854577@qq.com>
2017-09-08 18:22:39 +0800

    路由已经配置好

---
+++ src/components/Posts/posts.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="posts">
+        <Subnav :secondLevel="secondLevel" :threeLevel="threeLevel" @refresh="refresh"></Subnav>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name:'Posts',
+        components:{
+            Subnav,
+            BigDialog,
+            AreaAll
+        },
+        data() {
+            return {
+
+            }
+        },
+        filters:{
+
+        },
+        computed:{
+
+        },
+        created(){
+
+        },
+        methods: {
+
+        },
+        mounted(){
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
+<style>
+
+</style>
